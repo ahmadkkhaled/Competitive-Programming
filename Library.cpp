69dd17ddee80297cf3eadc57c103ee6a2f2ae6e2
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2019-09-13 20:33:58 +0200

    Update Library.cpp

--- Library.cpp
+++ Library.cpp
@@ -27,8 +27,16 @@ bool merge(int u , int v){
 
 ///**************************************************************************************
 
-
  long long choose(long long n, long long k){
      if(!k) return 1;
      return n*choose(n-1,k-1)/k;
+}
+
+///**************************************************************************************
+
+const int N = 1e3+1; ///1e4*1e4 integer matrix is 400 MB
+long long choose[N][N];
+void initpascal(){ /// Pascal's triangle for nChoosek problems
+    for(int i=0; i<N; ++i) choose[i][0] = choose[i][i] = 1;
+    for(int i=2; i<N; ++i) for(int j=1; j<i; ++j) choose[i][j] = choose[i-1][j] + choose[i-1][j-1];
 }
