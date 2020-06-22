395c8644997a595ed01f60c931036c601d205cfa
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2020-06-22 11:26:45 +0200

    Updated Library and Template

--- Library/DSU.cpp
+++ Library/DSU.cpp
@@ -16,3 +16,36 @@ bool merge(int u , int v){
     sz[v] += sz[u];
     return true;
 }
+
+/// INCOMPLETE
+/// requires testing
+class disjoint_set{
+private:
+    int *sz;
+    int *p;
+
+    disjoint_set(int size){
+        sz = new int[size];
+        p =  new int[size];
+        iota(p , p + size , 0); // 0 1 2 3 .. N
+        fill(sz , sz + size , 1);
+    }
+public:
+    int f(int u){ return p[u] = (p[u] == u ? u : f(p[u])); }
+    bool merge(int u , int v){
+        u = f(u);
+        v = f(v);
+
+        if(u == v)return false;
+
+        if(sz[v] < sz[u])swap(u , v);
+
+        p[u] = v;
+        sz[v] += sz[u];
+        return true;
+    }
+    ~disjoint_set(){
+        delete[] sz;
+        delete[] p;
+    }
+};
