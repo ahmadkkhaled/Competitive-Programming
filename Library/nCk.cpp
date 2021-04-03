99376eb34831d28aced74b5eebae8d466a105ac9
Ahmad Khaled <36166958+ahmadkkhaled@users.noreply.github.com>
2021-04-03 12:05:46 +0200

    Fixed overflow bug with nCk pascal

--- Library/nCk.cpp
+++ Library/nCk.cpp
@@ -9,13 +9,14 @@ long long choose(long long n, long long k){
 /**
  * nCk
  * Complexity O(n^2)
+ * OVERFLOWS WITHOUT MOD
  */
-const int N = 1e3+1; ///1e8 (1e4*1e4) integer matrix is 400 MB
+const int N = 1e3+1; ///1e3 * 1e3 is the max acceptable size
 long long choose[N][N];
 void initpascal(){
     for(int i=0; i<N; ++i)
         choose[i][0] = choose[i][i] = 1;
     for(int i=2; i<N; ++i)
         for(int j=1; j<i; ++j)
-            choose[i][j] = choose[i-1][j]+choose[i-1][j-1];
+            choose[i][j] = (choose[i-1][j]+choose[i-1][j-1]) % MOD;
 }
